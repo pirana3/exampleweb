7b3d20f41eb141366b9195a3e2935014440f2d58
pirana3 <piran356@gmail.com>
2024-02-08 04:07:41 -0600

    more standard changes

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,42 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  darkMode: 'class', // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'royal-blue': '#3A3AFC',
+        'royal-purple': '#5D3FFC',
+        'neon-blue': '#00E3FF',
+        'neon-purple': '#C600FF',
+      },
+      fontFamily: {
+        roboto: ['Roboto', 'sans-serif'],
+        
+      },
+      
+    },
+    screens: {
+      'sm': {'min': '300px', 'max': '767px'},
+      // => @media (min-width: 640px and max-width: 767px) { ... }
+
+      'md': {'min': '768px', 'max': '1023px'},
+      // => @media (min-width: 768px and max-width: 1023px) { ... }
+
+      'lg': {'min': '1024px', 'max': '1279px'},
+      // => @media (min-width: 1024px and max-width: 1279px) { ... }
+
+      'xl': {'min': '1280px', 'max': '1535px'},
+      // => @media (min-width: 1280px and max-width: 1535px) { ... }
+
+      '2xl': {'min': '1536px'},
+      // => @media (min-width: 1536px) { ... }
+    },
   },
+  variants: {
+    extend: {
+        display: ["group-hover"],
+    },
+},
   plugins: [],
 }
-
